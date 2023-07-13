9728c8aa8d8752564ad8af8e46165e2816f2ce50
AbdulBari <abdulbarialkasser1999@gmail.com>
2023-07-13 16:47:35 +0300

    more abstract

--- Extracted_faces.py
+++ Extracted_faces.py
@@ -1,17 +1,17 @@
 import numpy as np
 import face_recognition as fr
 
-class Face:
 
-    def __init__(self, frame_id, frame, face_location, prob=None):
-        self.frame_id = frame_id
+class ExtractedFace:
+    def __init__(self, id_frame, frame, face_location, prob=None):
+        self.id_last_frame = id_frame
         self.last_frame = frame
         
         self.last_face_location = face_location
         self.last_face_image = self.__cut_face()    # for testing # WARNING IS POINTER from frame not copy
         self.last_face_encoding = self.__encode_image()
         
-        self.probability = prob
+        self.last_face_probability = prob
     
     def __encode_image(self):
         face_encoding = fr.face_encodings(self.last_frame, [self.last_face_location])
@@ -21,44 +21,49 @@ class Face:
     def __cut_face(self):
         t, r, b, l = self.last_face_location
         return self.last_frame[l:r,t:b]
-
-
-class ExtractedFace(Face):
-    def __init(self, frame_id, frame, face_location, prob=None):
-        pass
         
+    def match_by_encodings(self, known_faces_encodings:list, tolerance=0.6) -> list:
+        matches = fr.compare_faces(known_faces_encodings, self.last_face_encoding, tolerance=tolerance)
+        return matches
+    
+    def distance_by_encodings(self, known_faces_encodings:list) -> list:
+        res = fr.face_distance(known_faces_encodings, self.last_face_encoding)
+        return res
 
 class TrackedFace:
     
-    THRESHOLD = 0.2
     ID = 0
 
-    def __init__(self, frame_id, frame, face_image, face_location, prob):
+    def __init__(self, face:ExtractedFace):
         self.ID = TrackedFace.ID
+
+        self.__dict__.update(vars(face))
+        
+        self.best_face_image = self.last_face_image
+        self.best_face_probability = self.last_face_probability
+
         self.duration_existence = 1
-
         TrackedFace.ID += 1
+    
+    def update_info(self, face:ExtractedFace):
+        """call when sucess track"""
 
-    def increase_duration(self):
+        # self.last_frame = face.last_frame
+        # self.last_face_location = face.last_face_location
+        # self.last_face_image = face.last_face_image
+        # self.last_face_encoding = face.last_face_encoding
+        self.__dict__.update(vars(face))
+        
+        if self.best_face_probability <= self.last_face_probability:
+            self.best_face_image = self.last_face_image
+            self.best_face_probability = self.last_face_probability
+        
         self.duration_existence += 1
-
-    def set_last_face_image(self, face_image):
-        self.last_face_image = face_image
-
-    def set_best_face_image(self, face_image):
-        self.best_face_image = face_image
-
-    def similarity(first_obj:"TrackedFace", second_obj:"TrackedFace"):
-        return np.dot(first_obj.features_encoding, second_obj.features_encoding) / (
-            np.linalg.norm(first_obj.features_encoding)*np.linalg.norm(second_obj.features_encoding)
-            )
-    
-    def __eq__(self, another_obj:"TrackedFace"):
-        return TrackedFace.similarity(self, another_obj) >= TrackedFace.THRESHOLD
-    
-    def update_info(self, obj:"TrackedFace"):
-        # self.set_last_frame(obj.last_frame)
-        # self.set_last_face_image(obj.last_face_image)
-        # self.set_last_face_location(obj.last_face_location)
-        # self.set_last_face_encoding(obj.last_face_encoding)
         pass
+
+
+    # def similarity(first_obj:"TrackedFace", second_obj:"TrackedFace"):
+    #     return np.dot(first_obj.features_encoding, second_obj.features_encoding) / (
+    #         np.linalg.norm(first_obj.features_encoding)*np.linalg.norm(second_obj.features_encoding)
+    #         )
+    
