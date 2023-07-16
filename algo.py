47e18efdd32376d1f509ef922040b88764c9d002
AbdulBari <abdulbarialkasser1999@gmail.com>
2023-07-16 05:02:35 +0300

    complete obb

--- algo.py
+++ algo.py
@@ -14,134 +14,134 @@ from os.path import join
 from detector_model import Detector, FaceReconationDetecor, MediaPipeDetector
 from typing import Union
 
-def get_info_result(detection_result, frame):
-    faces_image = locations = prob = []
-    for detection in detection_result.detections:
-        bbox = detection.bounding_box   
-        top, left = bbox.origin_x, bbox.origin_y
-        bottom, right = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-        faces_image.append(
-            frame[left:right, top:bottom].copy()
-        )
-        locations.append(
-            (top, right, bottom, left)
-        )
-        prob.append(detection.categories[0].score)
-    return faces_image, locations, prob
+# def get_info_result(detection_result, frame):
+#     faces_image = locations = prob = []
+#     for detection in detection_result.detections:
+#         bbox = detection.bounding_box   
+#         top, left = bbox.origin_x, bbox.origin_y
+#         bottom, right = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+#         faces_image.append(
+#             frame[left:right, top:bottom].copy()
+#         )
+#         locations.append(
+#             (top, right, bottom, left)
+#         )
+#         prob.append(detection.categories[0].score)
+#     return faces_image, locations, prob
 
 
-def detect_faces(id_frame, frame):
-    face_locations = fr.face_locations(frame)
-    return [ExtractedFace(id_frame, frame, face_location_set) for face_location_set in face_locations]
+# def detect_faces(id_frame, frame):
+#     face_locations = fr.face_locations(frame)
+#     return [ExtractedFace(id_frame, frame, face_location_set) for face_location_set in face_locations]
 
 
-def save_all_faces(all_faces:list[TrackedFace], fps):
-    if not os.path.exists(SAVE_IMAGE_PATH):
-        os.mkdir(SAVE_IMAGE_PATH)
-    # data as csv
-    print(f"length data: {len(all_faces)}")
-    data = [face_set.to_csv() for face_set in all_faces]
-    df = pd.DataFrame(data, columns=data[0].keys())
+# def save_all_faces(all_faces:list[TrackedFace], fps):
+#     if not os.path.exists(SAVE_IMAGE_PATH):
+#         os.mkdir(SAVE_IMAGE_PATH)
+#     # data as csv
+#     print(f"length data: {len(all_faces)}")
+#     data = [face_set.to_csv() for face_set in all_faces]
+#     df = pd.DataFrame(data, columns=data[0].keys())
     
-    df["start_time"] =  df.start_index_frame.apply(index_to_time, fps=fps)
-    df["end_time"] =  df.end_index_frame.apply(index_to_time, fps=fps)
+#     df["start_time"] =  df.start_index_frame.apply(index_to_time, fps=fps)
+#     df["end_time"] =  df.end_index_frame.apply(index_to_time, fps=fps)
 
-    df.to_csv(SAVE_DF_PATH, index=False)
+#     df.to_csv(SAVE_DF_PATH, index=False)
 
-    # save best faces
-    image_paths = [join(SAVE_IMAGE_PATH, face_set.get_unique_name() + ".jpg")  for face_set in all_faces] 
-    images = [face_set.best_face_image for face_set in all_faces]
-    for path, image in zip(image_paths, images):
-        save_image(image[:,:,::-1], path)
-
-
+#     # save best faces
+#     image_paths = [join(SAVE_IMAGE_PATH, face_set.get_unique_name() + ".jpg")  for face_set in all_faces] 
+#     images = [face_set.best_face_image for face_set in all_faces]
+#     for path, image in zip(image_paths, images):
+#         save_image(image[:,:,::-1], path)
 
 
 
 
 
 
-# Create an FaceDetector object.
-# VisionRunningMode = mp.tasks.vision.RunningMode
-# base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
-# options = vision.FaceDetectorOptions(base_options=base_options, running_mode=VisionRunningMode.VIDEO)
-# detector = vision.FaceDetector.create_from_options(options)
 
-all_faces, tracked_faces, current_faces = [], [], []
 
-# Use OpenCV’s VideoCapture to load the input video.
-vid = cv2.VideoCapture(VIDEO_PATH)
+# # Create an FaceDetector object.
+# # VisionRunningMode = mp.tasks.vision.RunningMode
+# # base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
+# # options = vision.FaceDetectorOptions(base_options=base_options, running_mode=VisionRunningMode.VIDEO)
+# # detector = vision.FaceDetector.create_from_options(options)
 
-# Load the frame rate of the video
-fps = vid.get(cv2.CAP_PROP_FPS)
-print(f"{fps = }")
+# all_faces, tracked_faces, current_faces = [], [], []
 
-detector = FaceReconationDetecor()
+# # Use OpenCV’s VideoCapture to load the input video.
+# vid = cv2.VideoCapture(VIDEO_PATH)
 
-while(vid.isOpened()):
+# # Load the frame rate of the video
+# fps = vid.get(cv2.CAP_PROP_FPS)
+# print(f"{fps = }")
+
+# detector = FaceReconationDetecor()
+
+# while(vid.isOpened()):
       
-    # Capture the video frame
-    frame_exists, frame = vid.read()
+#     # Capture the video frame
+#     frame_exists, frame = vid.read()
     
-    # if frame is read correctly ret is True
-    # if not frame_exists: break
+#     # if frame is read correctly ret is True
+#     # if not frame_exists: break
 
-    # get fram position and frame millesecond time
-    num_frame = vid.get(cv2.CAP_PROP_POS_FRAMES)
-    ms = vid.get(cv2.CAP_PROP_POS_MSEC)
+#     # get fram position and frame millesecond time
+#     num_frame = vid.get(cv2.CAP_PROP_POS_FRAMES)
+#     ms = vid.get(cv2.CAP_PROP_POS_MSEC)
 
-    # to take frame in second
-    if num_frame % int(fps) : continue
-    # resize
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
+#     # to take frame in second
+#     if num_frame % int(fps) : continue
+#     # resize
+#     frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
 
-    # convert frame to RGB
-    frame = frame[:,:,::-1]
+#     # convert frame to RGB
+#     frame = frame[:,:,::-1]
 
-    # detect faces
-    current_faces = detector.detect_faces(num_frame, frame)
+#     # detect faces
+#     current_faces = detector.detect_faces(num_frame, frame)
 
-    # cv2.imshow("frame", frame[:,:,::-1])
-    # for i, face in enumerate(current_faces):
-    #     cv2.imshow(f'face image {i}', face.last_face_image)
+#     # cv2.imshow("frame", frame[:,:,::-1])
+#     # for i, face in enumerate(current_faces):
+#     #     cv2.imshow(f'face image {i}', face.last_face_image)
 
-    Faces_keep_appearing = []
-    if tracked_faces:
-        current_faces_encodings = [face.last_face_encoding for face in current_faces]
+#     Faces_keep_appearing = []
+#     if tracked_faces:
+#         current_faces_encodings = [face.last_face_encoding for face in current_faces]
 
-        # Compare between all last faces and all current_faces
-        for index, tracked_face_set in enumerate(tracked_faces):
-            tracked_face_set:TrackedFace
+#         # Compare between all last faces and all current_faces
+#         for index, tracked_face_set in enumerate(tracked_faces):
+#             tracked_face_set:TrackedFace
 
-            if len(current_faces_encodings) == 0: 
-                all_faces.append(tracked_face_set)
-                continue
+#             if len(current_faces_encodings) == 0: 
+#                 all_faces.append(tracked_face_set)
+#                 continue
 
-            matches = tracked_face_set.match_by_encodings(current_faces_encodings)
-            face_distances = tracked_face_set.distance_by_encodings(current_faces_encodings)
+#             matches = tracked_face_set.match_by_encodings(current_faces_encodings)
+#             face_distances = tracked_face_set.distance_by_encodings(current_faces_encodings)
 
-            best_match_index = np.argmin(face_distances)
+#             best_match_index = np.argmin(face_distances)
 
-            if matches[best_match_index]:
-                tracked_face_set.update_info(current_faces[best_match_index])
-                current_faces.pop(best_match_index)
-                current_faces_encodings.pop(best_match_index)
-                Faces_keep_appearing.append(tracked_face_set)
-            else:
-                all_faces.append(tracked_face_set)
+#             if matches[best_match_index]:
+#                 tracked_face_set.update_info(current_faces[best_match_index])
+#                 current_faces.pop(best_match_index)
+#                 current_faces_encodings.pop(best_match_index)
+#                 Faces_keep_appearing.append(tracked_face_set)
+#             else:
+#                 all_faces.append(tracked_face_set)
     
-    tracked_faces = Faces_keep_appearing
-    for face in current_faces:
-        tracked_faces.append(TrackedFace(face))
+#     tracked_faces = Faces_keep_appearing
+#     for face in current_faces:
+#         tracked_faces.append(TrackedFace(face))
 
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+#     if cv2.waitKey(1) == ord('q'):
+#         break
 
 
-vid.release()
-cv2.destroyAllWindows()
-save_all_faces(all_faces, fps)
+# vid.release()
+# cv2.destroyAllWindows()
+# save_all_faces(all_faces, fps)
 
 
 class FaceDetectionTimeTracker:
@@ -166,16 +166,16 @@ class FaceDetectionTimeTracker:
     def run(self):
         while(True):
             # Capture the video frame
-            frame_exists, frame = vid.read()
+            frame_exists, frame = self.vid.read()
             
             # if frame is read correctly ret is True
             if not frame_exists: break
 
             # get fram position and frame millesecond time
-            num_frame = vid.get(cv2.CAP_PROP_POS_FRAMES)
+            num_frame = self.vid.get(cv2.CAP_PROP_POS_FRAMES)
 
             # to take frame in second
-            if num_frame % int(self.n_sec * fps) : continue
+            if num_frame % int(self.n_sec * self.fps) : continue
             
             # resize
             if self.dsize: frame = cv2.resize(frame, self.dsize)
@@ -188,11 +188,11 @@ class FaceDetectionTimeTracker:
                 current_faces_encodings = [face.last_face_encoding for face in self.current_faces]
 
                 # Compare between all last faces and all current_faces
-                for index, tracked_face_set in enumerate(self.tracked_faces):
+                for tracked_face_set in self.tracked_faces:
                     tracked_face_set:TrackedFace
 
                     if len(current_faces_encodings) == 0: 
-                        all_faces.append(tracked_face_set)
+                        self.all_faces.append(tracked_face_set)
                         continue
 
                     matches = tracked_face_set.match_by_encodings(current_faces_encodings)
@@ -211,3 +211,22 @@ class FaceDetectionTimeTracker:
             self.tracked_faces = Faces_keep_appearing
             for face in self.current_faces:
                 self.tracked_faces.append(TrackedFace(face))
+    
+    def save_data(self):
+        if not os.path.exists(SAVE_IMAGE_PATH):
+            os.mkdir(SAVE_IMAGE_PATH)
+        # data as csv
+        print(f"length data: {len(self.all_faces)}")
+        data = [face_set.to_csv() for face_set in self.all_faces]
+        df = pd.DataFrame(data, columns=data[0].keys())
+        
+        df["start_time"] =  df.start_index_frame.apply(index_to_time, fps=self.fps)
+        df["end_time"] =  df.end_index_frame.apply(index_to_time, fps=self.fps)
+
+        df.to_csv(SAVE_DF_PATH, index=False)
+
+        # save best faces
+        image_paths = [join(SAVE_IMAGE_PATH, face_set.get_unique_name() + ".jpg")  for face_set in self.all_faces] 
+        images = [face_set.best_face_image for face_set in self.all_faces]
+        for path, image in zip(image_paths, images):
+            save_image(image[:,:,::-1], path)
