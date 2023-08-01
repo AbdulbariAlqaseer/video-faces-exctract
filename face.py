a7059f324e6f7dc4a5a43e7294eebec982b95089
AbdulBari <abdulbarialkasser1999@gmail.com>
2023-08-01 21:10:58 +0300

    remove ExtractedFace and merge with TrackedFace

--- face.py
+++ face.py
@@ -4,16 +4,48 @@ import cv2 as cv
 from utile import index_to_time
 # from deepface import DeepFace
 
-class ExtractedFace:
-    def __init__(self, id_frame, frame, face_location,face_box=None, prob=None):
-        self.id_last_frame = id_frame
+# class ExtractedFace:
+#     def __init__(self, frame, face_location, id_frame=None, face_box=None, prob=None):
+#         self.id_last_frame = id_frame
+#         self.last_frame = frame
+#         self.face_box = face_box
+#         self.last_face_location = face_location
+#         self.last_face_image = self.__cut_face()    # for testing # WARNING IS POINTER from frame not copy
+#         self.last_face_encoding = self.__encode_image()
+        
+#         self.last_face_probability = prob
+    
+#     def __encode_image(self):
+#         face_encoding = fr.face_encodings(self.last_frame, [self.last_face_location])
+#         if len(face_encoding) > 1: raise Exception("found more than face in location")
+#         return face_encoding[0]
+
+#     def __cut_face(self):
+#         t, r, b, l = self.last_face_location
+#         return self.last_frame[t:b, l:r]
+        
+
+class TrackedFace:
+    
+    ID = 0
+
+    def __init__(self, frame, face_location, id_frame=None, prob=None):
+        self.face_ID = TrackedFace.ID
+
+        self.id_last_frame = self.id_first_frame = id_frame
+        self.duration_existence = 0
+
         self.last_frame = frame
-        self.face_box = face_box
         self.last_face_location = face_location
         self.last_face_image = self.__cut_face()    # for testing # WARNING IS POINTER from frame not copy
         self.last_face_encoding = self.__encode_image()
-        
         self.last_face_probability = prob
+
+        self.best_face_image = self.last_face_image
+        self.best_face_probability = self.last_face_probability
+        self.best_face_encoding = self.last_face_encoding
+
+        TrackedFace.ID += 1
     
     def __encode_image(self):
         face_encoding = fr.face_encodings(self.last_frame, [self.last_face_location])
@@ -23,36 +55,37 @@ class ExtractedFace:
     def __cut_face(self):
         t, r, b, l = self.last_face_location
         return self.last_frame[t:b, l:r]
-        
 
-class TrackedFace:
-    
-    ID = 0
-
-    def __init__(self, face:ExtractedFace):
-        self.face_ID = TrackedFace.ID
-
-        assert isinstance(face, ExtractedFace)
-        self.__dict__.update(vars(face))
-        
-        self.id_first_frame = self.id_last_frame
-        
-        self.best_face_image = self.last_face_image
-        self.best_face_probability = self.last_face_probability
-        self.best_face_encoding = self.last_face_encoding
-
-        self.duration_existence = 0
-        TrackedFace.ID += 1
-    
-    def update_info(self, face:ExtractedFace):
+    def update_info(self, face:"TrackedFace"):
         """call when sucess track"""
-        assert isinstance(face, ExtractedFace)
-        self.__dict__.update(vars(face))
+        assert isinstance(face, TrackedFace)
+
+        """
+            self.id_last_frame          Y
+            self.id_first_frame         N
+            self.duration_existence     calc
+            
+            self.last_frame             Y
+            self.last_face_location     Y
+            self.last_face_image        Y
+            self.last_face_encoding     Y
+            self.last_face_probability  Y
+
+            self.best_face_image        calc
+            self.best_face_probability  calc
+            self.best_face_encoding     calc
+        """
+        self.id_last_frame = face.id_last_frame
+        self.last_frame = face.last_frame
+        self.last_face_location = face.last_face_location
+        self.last_face_image = face.last_face_image
+        self.last_face_encoding = face.last_face_encoding
+        self.last_face_probability = face.last_face_probability
         
-        if self.best_face_probability and self.best_face_probability <= self.last_face_probability:
+        if self.best_face_probability and self.best_face_probability <= face.last_face_probability:
             self.best_face_image = self.last_face_image
-            self.best_face_probability = self.last_face_probability
             self.best_face_encoding = self.last_face_encoding
+            self.best_face_probability = self.last_face_probability
         
         self.duration_existence = self.id_last_frame - self.id_first_frame 
 
@@ -76,7 +109,7 @@ class TrackedFace:
             "face_image" : self.best_face_image 
         }
 
-    
+'''
 class FaceTrack:
     
     ID = 0
@@ -161,3 +194,5 @@ class FaceTrack:
             "unique_name" : self.get_unique_name()
         }
         return d
+
+'''
